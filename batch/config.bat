656e08a4f2b8155e75d59a25a5e83fb464d5eec5
Matúš Kalanin <mato.kalanin@gmail.com>
2025-03-31 18:42:11 +0200

    Update config.bat

--- batch/config.bat
+++ batch/config.bat
@@ -1,10 +1,10 @@
-if [ "$1" == "delete" ]; then
-	kubectl  delete -f ./plugins/config/scheduler-config-configmap.yaml
-	kubectl  delete -f ./plugins/config/rbac.yaml
-	kubectl  delete -f ./middleware/config/kuberlbridge-service.yaml
-
-else
-	kubectl  apply -f ./plugins/config/scheduler-config-configmap.yaml
-	kubectl  apply -f ./plugins/config/rbac.yaml
-	kubectl  apply -f ./middleware/config/kuberlbridge-service.yaml
-fi
+@echo off
+if "%1"=="delete" (
+    kubectl delete -f ./plugins/config/scheduler-config-configmap.yaml
+    kubectl delete -f ./plugins/config/rbac.yaml
+    kubectl delete -f ./middleware/config/kuberlbridge-service.yaml
+) else (
+    kubectl apply -f ./plugins/config/scheduler-config-configmap.yaml
+    kubectl apply -f ./plugins/config/rbac.yaml
+    kubectl apply -f ./middleware/config/kuberlbridge-service.yaml
+)
