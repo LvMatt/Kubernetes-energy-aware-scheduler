748f6c2d4ea1baed1973117db9b5077879a5b424
Matúš Kalanin <mato.kalanin@gmail.com>
2025-04-04 00:01:24 +0200

    Handle multiple nodes at score request

--- middleware/kubeRLBridge.js
+++ middleware/kubeRLBridge.js
@@ -5,26 +5,47 @@ const app = express();
 const PORT = 3000;
 
 app.get("/score", async (req, res) => {
-    const nodeName = req.query?.nodeName;
-    const podName = req.query?.podName;
-    const podPriority = req.query?.podPriority;
-    const podSchedulerNameAffilation = req.query?.podSchedulerNameAffilation;
+    try {
+        
+        const nodesRaw = req.query?.nodes;
+        const podName = req.query?.podName;
+        const podPriority = req.query?.podPriority;
+        const podSchedulerNameAffilation = req.query?.podSchedulerNameAffilation;
+        console.log("nodesRaw", nodesRaw)
+        console.log("podName", podName)
+        if(!nodesRaw || !podName || !podPriority || !podSchedulerNameAffilation ) { 
+            return res.json({
+                "errorMsg": 'Parameters are missing',
+                'bestNode': '',
+                'statusCode': 400
+            });
+        }    
+        let nodesArr;
+        nodesArr = nodesRaw.split(',');
+        // if(nodesRaw.includes('[') && nodesRaw.includes(']')  ) nodesArr = nodesRaw.replace('[', '').replace(']', '');
 
-    if(!nodeName || !podName || !podPriority || !podSchedulerNameAffilation ) res.json({
-        'score': 0,
-        'node': '',
-        'statusCode': 400
-    })
-    
-    let energyUsage = Math.floor(Math.random() * 100); 
-    let score = 100 - energyUsage;
+        const nodesLen = nodesArr.length;
+        console.log("nodesLen", nodesLen)
+        console.log("nodes:::--__", nodesArr)
 
-    console.log(`Node: ${nodeName}, Energy Usage: ${energyUsage}, Score: ${score}`);
-    res.json({
-        'score': score,
-        'node': nodeName,
-        'statusCode': 200
-    });
+        let energyUsage = Math.floor(Math.random() * 100); 
+        let score = 100 - energyUsage;
+
+        console.log(`Node: ${nodesRaw}, Energy Usage: ${energyUsage}, Score: ${score}`);
+        return res.json({
+            "errorMsg": '',
+            'bestNode': nodesArr[0],
+            'statusCode': 200
+        });
+    }
+    catch (err) {
+        console.warn("err____", String(err));
+        return res.json({
+            'errMsg': String(err),
+            'bestNode': '',
+            'statusCode': 400
+        });
+    }
 });
 
 // Start the server
