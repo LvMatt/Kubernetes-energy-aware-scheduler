3dcf72e6703e40d216789cde284dd4a56a0aa296
Matúš Kalanin <mato.kalanin@gmail.com>
2025-04-21 15:39:44 +0200

    Observer service + deployment

--- bash/config.sh
+++ bash/config.sh
@@ -2,9 +2,10 @@ if [ "$1" == "delete" ]; then
 	kubectl  delete -f ./plugins/config/scheduler-config-configmap.yaml
 	kubectl  delete -f ./plugins/config/rbac.yaml
 	kubectl  delete -f ./middleware/config/kuberlbridge-service.yaml
-
+	kubectl  delete -f ./observer/config/observer-service.yaml
 else
 	kubectl  apply -f ./plugins/config/scheduler-config-configmap.yaml
 	kubectl  apply -f ./plugins/config/rbac.yaml
 	kubectl  apply -f ./middleware/config/kuberlbridge-service.yaml
+	kubectl  apply -f ./observer/config/observer-service.yaml
 fi
--- bash/deploy.sh
+++ bash/deploy.sh
@@ -2,8 +2,10 @@ if [ "$1" == "delete" ]; then
 	kubectl delete -f ./middleware/deploy/kuberlbridge-deployment.yaml -n kube-system
 	kubectl delete -f ./plugins/deploy/energy-scheduler-deployment.yaml -n kube-system
 	kubectl delete -f ./plugins/config/test-pod.yaml
+	kubectl delete -f ./observer/deploy/observer-deployment.yaml -n kube-system
 else 
 	kubectl apply -f ./middleware/deploy/kuberlbridge-deployment.yaml -n kube-system
 	kubectl apply -f ./plugins/deploy/energy-scheduler-deployment.yaml -n kube-system
 	kubectl apply -f ./plugins/config/test-pod.yaml
+	kubectl apply -f ./observer/deploy/observer-deployment.yaml -n kube-system
 fi
