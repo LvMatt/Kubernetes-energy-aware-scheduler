29d22ffdbdb274f7d8436edb8e98fe5ec486456b
Matúš Kalanin <mato.kalanin@gmail.com>
2025-04-22 23:25:02 +0200

    Script for draining nodes

---
+++ bash/drain.sh
@@ -0,0 +1,3 @@
+kubectl drain energy-aware-k8-cluster-worker --ignore-daemonsets --delete-emptydir-data
+kubectl drain energy-aware-k8-cluster-worker2 --ignore-daemonsets --delete-emptydir-data
+kubectl drain energy-aware-k8-cluster-worker3 --ignore-daemonsets --delete-emptydir-data
