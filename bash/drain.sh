bf6beba6bc2c87b8fc10f819adef0a7f1e8a5cc7
Matúš Kalanin <mato.kalanin@gmail.com>
2025-04-22 23:34:20 +0200

    uncordon nodes after draining

--- bash/drain.sh
+++ bash/drain.sh
@@ -1,3 +1,8 @@
 kubectl drain energy-aware-k8-cluster-worker --ignore-daemonsets --delete-emptydir-data
 kubectl drain energy-aware-k8-cluster-worker2 --ignore-daemonsets --delete-emptydir-data
 kubectl drain energy-aware-k8-cluster-worker3 --ignore-daemonsets --delete-emptydir-data
+
+
+kubectl uncordon energy-aware-k8-cluster-worker
+kubectl uncordon energy-aware-k8-cluster-worker2
+kubectl uncordon energy-aware-k8-cluster-worker3
