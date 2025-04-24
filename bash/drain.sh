fbc21865118e8bb66982db00eafe7f9329e62094
Matúš Kalanin <mato.kalanin@gmail.com>
2025-04-24 10:36:48 +0200

    Force drain

--- bash/drain.sh
+++ bash/drain.sh
@@ -1,7 +1,6 @@
-kubectl drain energy-aware-k8-cluster-worker --ignore-daemonsets --delete-emptydir-data
-kubectl drain energy-aware-k8-cluster-worker2 --ignore-daemonsets --delete-emptydir-data
-kubectl drain energy-aware-k8-cluster-worker3 --ignore-daemonsets --delete-emptydir-data
-
+kubectl drain energy-aware-k8-cluster-worker --ignore-daemonsets --delete-emptydir-data --force
+kubectl drain energy-aware-k8-cluster-worker2 --ignore-daemonsets --delete-emptydir-data --force
+kubectl drain energy-aware-k8-cluster-worker3 --ignore-daemonsets --delete-emptydir-data --force
 
 kubectl uncordon energy-aware-k8-cluster-worker
 kubectl uncordon energy-aware-k8-cluster-worker2
