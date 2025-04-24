8a38d2a560c0c89fecfdc9a99f790b439d4e7b47
Matúš Kalanin <mato.kalanin@gmail.com>
2025-04-24 10:37:46 +0200

    Uploading new RL models

--- RL-service/rl_scheduler_api.py
+++ RL-service/rl_scheduler_api.py
@@ -9,8 +9,11 @@ app = FastAPI()
 
 # Load the trained RL model
 # MODEL_PATH_PREVIOUS = "RL-service/dqn_kubernetes"
-MODEL_PATH = "dqn_kubernetes.zip"
-model = DQN.load(MODEL_PATH)
+MODEL_CPU_PATH = "dqn_kubernetes_cpu.zip"
+MODEL_ENERGY_PATH = "dqn_kubernetes_energy.zip"
+model_cpu = DQN.load(MODEL_CPU_PATH)
+model_energy = DQN.load(MODEL_ENERGY_PATH)
+
 
 # Match the middleware's JSON format
 class CPU(BaseModel):
@@ -25,18 +28,18 @@ def schedule_from_middleware(nodes: list[NodeData]):
     # Get CPU usage from each node
     print("Received nodes data:", nodes)
     cpu_usages = [node.cpu.used_percent / 100.0 for node in nodes]  # normalize to [0, 1]
-
+    print("cpu_usages", cpu_usages)
     # Pad or truncate to 5 elements (model was trained on 3)
     state = np.zeros(3, dtype=np.float32)
     for i in range(min(3, len(cpu_usages))):
         state[i] = cpu_usages[i]
 
     # Predict the action
-    action, _ = model.predict(state)
+    action, _ = model_cpu.predict(state)
 
     # Safety: If action index is out of bounds, default to first node
     chosen_node = nodes[action].node if action < len(nodes) else nodes[0].node
-
+    print("Chosen node:", chosen_node)
     return {"scheduled_node": chosen_node}
 
 # Health check endpoint
