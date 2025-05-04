a8bf6f339a6f4c603b22a7775d53bac864d2bcc6
Matúš Kalanin <mato.kalanin@gmail.com>
2025-05-04 15:26:00 +0200

    Extend with energy usage

--- RL-service/rl_scheduler_api.py
+++ RL-service/rl_scheduler_api.py
@@ -9,9 +9,9 @@ app = FastAPI()
 
 # Load the trained RL model
 # MODEL_PATH_PREVIOUS = "RL-service/dqn_kubernetes"
-MODEL_CPU_PATH = "dqn_kubernetes_cpu.zip"
+MODEL_MEMORY_PATH = "dqn_kubernetes_cpu.zip"
 MODEL_ENERGY_PATH = "dqn_kubernetes_energy.zip"
-model_cpu = DQN.load(MODEL_CPU_PATH)
+model_memory = DQN.load(MODEL_MEMORY_PATH)
 model_energy = DQN.load(MODEL_ENERGY_PATH)
 
 
@@ -28,20 +28,29 @@ class NodeData(BaseModel):
 
 @app.post("/schedule")
 def schedule_from_middleware(nodes: list[NodeData]):
-    # Get CPU usage from each node
     print("Received nodes data:", nodes)
-    cpu_usages = [node.cpu.used_percent / 100.0 for node in nodes]  # normalize to [0, 1]
-    ram_usages = [node.memory.used_percent / 100.0 for node in nodes]  # normalize to [0, 1]
-    print("ram_usages", ram_usages)
+    #cpu_usages = [node.cpu.used_percent / 100.0 for node in nodes]  # normalize to [0, 1]
+    memory_usages = [node.memory.used_percent / 100.0 for node in nodes]
+    active_pods = [node.activePods.active_pods for node in nodes]
+
+    print("memory_usages", memory_usages)
     # Pad or truncate to 5 elements (model was trained on 3)
     state = np.zeros(3, dtype=np.float32)
-    for i in range(min(3, len(ram_usages))):
-        state[i] = ram_usages[i]
+    for i in range(min(3, len(memory_usages))):
+        state[i] = memory_usages[i]
 
-    # Predict the action
-    action, _ = model_cpu.predict(state)
 
-    # Safety: If action index is out of bounds, default to first node
+    # Energy usage prediction
+    """ for mem, pods in zip(memory_usages, active_pods):
+        state.extend([mem, pods]) """
+    
+    """ state = (state + [0.0] * 6)[:6]
+    state_np = np.array(state, dtype=np.float32)
+    action, _ = model_memory.predict(state_np) """
+
+    # Memory usage prediction
+    action, _ = model_memory.predict(state)
+
     chosen_node = nodes[action].node if action < len(nodes) else nodes[0].node
     print("Chosen node:", chosen_node)
     return {"scheduled_node": chosen_node}
